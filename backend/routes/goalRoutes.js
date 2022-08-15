b81d9b449d6b2689f65e8e4d055c05d58ffdb130
deepankar008 <87903654+deepankar008@users.noreply.github.com>
2022-08-15 15:26:32 +0000

    fcommit

---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  getGoals,
+  postGoal,
+  putGoal,
+  deleteGoal,
+} = require("../controllers/goalController");
+
+const { protect } = require("../middleware/authMiddleware");
+router.route("/").get(protect, getGoals).post(protect, postGoal);
+router.route("/:id").put(protect, putGoal).delete(protect, deleteGoal);
+
+module.exports = router;
+
+// router.get("/",getGoals);
+// no touching anymore
